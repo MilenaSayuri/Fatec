98cf9392872b9e874b7fa0afff148e8680b79c0b
Caio Diogenes <kaitohiraga2015@gmail.com>
2022-09-28 17:42:52 -0300

    Adicionando codigo para fatorial em C

---
+++ Fatorial.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {  
+    int n=0, i=0, fat=1;
+    printf("Entre com um valor n: ");
+    scanf("%d", &n);
+ 
+    fat = n;
+    for(i=1; i<n; i++){ fat = fat*i; }
+    
+    printf("\nO fatorial de %d é %d", n, fat);
+    return 0;
+}
